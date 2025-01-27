47aacb43df4f730437f900c66771109c34f9ebfe
Rahulcheryala <rahulcheryala2004@gmail.com>
2025-01-27 18:28:09 +0530

    project start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Hello, Tailwind CSS 4.0!
-      </h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold text-blue-500">Hello World !</h1>
+      <br />
+      <p className="text-lg text-gray-600 mt-4">Let's build something cool.</p>
     </div>
   );
 };
